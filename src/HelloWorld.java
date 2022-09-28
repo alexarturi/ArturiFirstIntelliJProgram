a3c0f095611339b39922ee12deb6da02a0c50b93
BT_3W2_01 <alexandrea8@nycstudents.net>
2022-09-28 11:44:35 -0400

    added a print statement to line 6

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main (String []args){
+        System.out.println("Hello World");
+        System.out.println("This is my first IntelliJ Program.");
+        System.out.println("I am very excited!");
+        System.out.println("This is a new line.");
+    }
 }
